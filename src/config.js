0d4f95b776271d644139fc748b47f0c333b5bc9b
Srikanth Reddy <srikanthreddy@Srikanths-MacBook-Pro.local>
2021-12-06 10:17:30 +0530

    config update.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const config = {
     RPC_URL: 'https://rpc.comdex.one',
     REST_URL: 'https://rest.comdex.one',
-    EXPLORER_URL: 'http://45.72.100.112:3000',
+    EXPLORER_URL: null,
     NETWORK_NAME: 'Comdex',
     NETWORK_TYPE: 'mainnet',
     CHAIN_ID: 'comdex-1',
@@ -11,7 +11,7 @@ export const config = {
     COIN_DECIMALS: 6,
     PREFIX: 'comdex',
     COIN_TYPE: 118,
-    GAS_PRICE_STEP_LOW: 0.01,
-    GAS_PRICE_STEP_AVERAGE: 0.025,
-    GAS_PRICE_STEP_HIGH: 0.04,
+    GAS_PRICE_STEP_LOW: 0.1,
+    GAS_PRICE_STEP_AVERAGE: 0.5,
+    GAS_PRICE_STEP_HIGH: 1,
 };
