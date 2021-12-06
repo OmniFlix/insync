908032a6d9d45724b7ec34adbd7c8eca8c638fc8
Srikanth Reddy <srikanthreddy@Srikanths-MacBook-Pro.local>
2021-12-06 10:22:55 +0530

    gas price updated.

--- src/config.js
+++ src/config.js
@@ -11,7 +11,7 @@ export const config = {
     COIN_DECIMALS: 6,
     PREFIX: 'omniflix',
     COIN_TYPE: 118,
-    GAS_PRICE_STEP_LOW: 0.01,
-    GAS_PRICE_STEP_AVERAGE: 0.025,
-    GAS_PRICE_STEP_HIGH: 0.04,
+    GAS_PRICE_STEP_LOW: 0.1,
+    GAS_PRICE_STEP_AVERAGE: 0.5,
+    GAS_PRICE_STEP_HIGH: 1,
 };
