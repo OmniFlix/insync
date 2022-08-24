8fe5be9d8162c08e07b7cc4c92c712acc3a4dfd2
srikanth.soparla <srikanth.soparla@cosmiclabs.co>
2022-08-24 13:53:57 +0530

    default gas value updates.

--- src/defaultGasValues.js
+++ src/defaultGasValues.js
@@ -1,8 +1,8 @@
 export const gas = {
     send: 100000,
     vote: 100000,
-    delegate: 180000,
-    un_delegate: 200000,
+    delegate: 250000,
+    un_delegate: 300000,
     re_delegate: 350000,
-    claim_reward: 200000,
+    claim_reward: 250000,
 };
