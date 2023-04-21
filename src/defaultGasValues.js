7f0d89d59e8a62bb41319d10083df6786667b7f0
Marri Harish <harishmarri551@gmail.com>
2023-04-22 03:21:41 +0530

    Update defaultGasValues.js

--- src/defaultGasValues.js
+++ src/defaultGasValues.js
@@ -1,8 +1,8 @@
 export const gas = {
-    send: 100000,
-    vote: 100000,
-    delegate: 200000,
-    un_delegate: 250000,
-    re_delegate: 350000,
-    claim_reward: 200000,
+    send: 150000,
+    vote: 150000,
+    delegate: 350000,
+    un_delegate: 350000,
+    re_delegate: 400000,
+    claim_reward: 250000,
 };
