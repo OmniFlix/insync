2258f2892777dbd97e5295953f4e012b7d842ceb
SrikanthSoparla <srikanth.soparla@gmail.com>
2023-07-10 11:09:08 +0530

    gas fee increased.

--- src/defaultGasValues.js
+++ src/defaultGasValues.js
@@ -1,8 +1,8 @@
 export const gas = {
     send: 80000,
     vote: 80000,
-    delegate: 160000,
-    un_delegate: 200000,
+    delegate: 200000,
+    un_delegate: 250000,
     re_delegate: 300000,
-    claim_reward: 140000,
+    claim_reward: 160000,
 };
