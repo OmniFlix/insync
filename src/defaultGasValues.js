f5c8b64218e9b9af8577988fe311f1c1520ed517
S.Srikanth Reddy <srikanth.soparla@gmail.com>
2024-10-17 09:12:03 +0530

    gas fee updated.

--- src/defaultGasValues.js
+++ src/defaultGasValues.js
@@ -1,8 +1,8 @@
 export const gas = {
     send: 150000,
     vote: 150000,
-    delegate: 350000,
-    un_delegate: 350000,
-    re_delegate: 600000,
-    claim_reward: 300000,
+    delegate: 400000,
+    un_delegate: 400000,
+    re_delegate: 650000,
+    claim_reward: 350000,
 };
