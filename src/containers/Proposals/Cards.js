062491bd7a4de50f6fdf25cc1c91de7235017c42
VasaviMahajan <laxmivasavimahajan@gmail.com>
2025-01-20 14:04:53 +0530

    description section updated
    Signed-off-by: = <=>

--- src/containers/Proposals/Cards.js
+++ src/containers/Proposals/Cards.js
@@ -119,17 +119,18 @@ const Cards = (props) => {
                                                     </Button>
                                                     : null}
                                     </div>
-                                    <p className="description">{content?.abstract}</p>
+                                    <p className="description">{(content?.abstract) || (content?.details)}</p>
                                     <div className="row">
                                         <div className="icon_info">
                                             <Icon className="person" icon="person"/>
                                             <span className="key_text">Proposer &nbsp;/&nbsp;
                                                 {inProgress
                                                     ? <DotsLoading/>
-                                                    : proposal && proposal.author && <div className="hash_text" title={proposal.author}>
+                                                    : proposal && proposal.author &&
+                                                    <div className="hash_text" title={proposal.author}>
                                                         <p className="name">{proposal.author}</p>
                                                         {proposal.author &&
-                                                        proposal.author.slice(proposal.author.length - 6, proposal.author.length)}
+                                                            proposal.author.slice(proposal.author.length - 6, proposal.author.length)}
                                                     </div>}
                                             </span>
                                         </div>
