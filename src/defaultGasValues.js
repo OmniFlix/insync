49ac92500a11cbd4722a8254c56ad16a79c1f0a6
S.Srikanth Reddy <srikanth.soparla@gmail.com>
2025-02-14 20:39:13 +0530

    Update defaultGasValues.js

--- src/defaultGasValues.js
+++ src/defaultGasValues.js
@@ -1,8 +1,8 @@
 export const gas = {
     send: 150000,
     vote: 150000,
-    delegate: 400000,
-    un_delegate: 400000,
-    re_delegate: 650000,
-    claim_reward: 350000,
+    delegate: 450000,
+    un_delegate: 450000,
+    re_delegate: 700000,
+    claim_reward: 450000,
 };
