dc99b5772d026543c1942a21e7ca4ded178de701
SrikanthSoparla <srikanth.soparla@gmail.com>
2025-03-03 12:55:02 +0530

    default gas updated to 500k

--- src/defaultGasValues.js
+++ src/defaultGasValues.js
@@ -1,8 +1,8 @@
 export const gas = {
     send: 150000,
     vote: 150000,
-    delegate: 450000,
-    un_delegate: 450000,
+    delegate: 500000,
+    un_delegate: 500000,
     re_delegate: 700000,
-    claim_reward: 450000,
+    claim_reward: 500000,
 };
