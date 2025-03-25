00f77b19e2812306354369cf04461e7447b7b874
aj2620 <97669418+aj2620@users.noreply.github.com>
2025-03-25 08:54:44 +0530

    fixed wasm issue (#123)

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     webpack: function override (config, env) {
@@ -11,21 +12,27 @@ module.exports = {
             buffer: require.resolve('buffer'),
             vm: false,
         };
+
         config.resolve.alias = {
             ...config.resolve.alias,
-            '../package.json': path.resolve(
-                __dirname,
-                'node_modules/@heliaxdev/namada-sdk/package.json',
-            ),
+            '../package.json': path.resolve(__dirname, 'node_modules/@namada/sdk/package.json'),
         };
-        config.resolve.alias = {
-            ...config.resolve.alias,
-            '../package.json': path.resolve(
-                __dirname,
-                'node_modules/@namada/sdk/package.json',
-            ),
-        };
+
         config.resolve.extensions = [...config.resolve.extensions, '.ts', '.js', '.wasm'];
+
+        // Enable WebAssembly support
+        config.experiments = {
+            asyncWebAssembly: true,
+            topLevelAwait: true,
+        };
+
+        // Add rule to properly handle .wasm files
+        config.module.rules.push({
+            test: /\.wasm$/,
+            type: 'asset/resource',
+        });
+
+        // Copy WASM file from node_modules/@namada/sdk/dist to the build folder
         config.plugins = [
             ...config.plugins,
             new HtmlWebpackPlugin({
@@ -42,7 +49,20 @@ module.exports = {
                     env: {},
                 },
             }),
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: path.resolve(__dirname, 'node_modules/@namada/sdk/dist/shared.namada.wasm'),
+                        to: '',
+                    },
+                    {
+                        from: path.resolve(__dirname, 'node_modules/@namada/sdk/dist/crypto.namada.wasm'),
+                        to: '',
+                    },
+                ],
+            }),
         ];
+
         config.ignoreWarnings = [/Failed to parse source map/];
 
         return config;
@@ -51,7 +71,7 @@ module.exports = {
     // Function to customize the devServer
     devServer: function (configFunction) {
         return function (proxy, allowedHost) {
-        // Get the default config and modify it
+            // Get the default config and modify it
             const config = configFunction(proxy, allowedHost);
 
             // Update devServer settings here
