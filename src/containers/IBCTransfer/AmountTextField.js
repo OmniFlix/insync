eb046eb40475245ecc6d28c4119da8560617d09d
poojachigitey <chigiteypooja12@gmail.com>
2025-05-30 16:58:32 +0530

    validation input fixes

--- src/containers/IBCTransfer/AmountTextField.js
+++ src/containers/IBCTransfer/AmountTextField.js
@@ -29,7 +29,7 @@ const AmountTextField = (props) => {
     }
    
     const handleChange = (input) => {
-        const value = parseFloat(input) || '';
+        const value = input === '' ? '' : parseFloat(input);
         const isValid = value <= amount;
         props.onChange(value, isValid);
     }
