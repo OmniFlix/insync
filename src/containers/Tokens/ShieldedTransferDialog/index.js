8d02e22b7187851f65ce2968292dc40c6ffa19f8
poojachigitey <chigiteypooja12@gmail.com>
2025-06-03 17:27:43 +0530

    proposal page feedbacks

--- src/containers/Tokens/ShieldedTransferDialog/index.js
+++ src/containers/Tokens/ShieldedTransferDialog/index.js
@@ -136,6 +136,9 @@ class ShieldedTransferDialog extends React.Component {
 
         const fromSelectedConfig = this.props.value && this.props.value.config && this.props.value.config.CHAIN_NAME ? this.props.value.config : null;
         const fee = feeList && feeList[fromSelectedConfig?.COIN_DENOM]
+
+        const disable = this.state.inProgress || this.props.tokensTransferAddressValid === false || !this.props.tokensTransferAmount || !this.props.tokensTransferAddress || this.props.tokensTransferAmountValid === false;
+
         return (
             <Dialog open={this.props.open}
             onClose={this.props.handleClose}
@@ -198,7 +201,7 @@ class ShieldedTransferDialog extends React.Component {
                     </div> : null}
                     {this.state.inProgress && <CircularProgress className="full_screen"/>}
                     <div className="actions">
-                        <Button disabled={this.state.inProgress} onClick={this.handleTransfer}>
+                        <Button disabled={disable} onClick={this.handleTransfer}>
                             {this.state.params
                                 ? 'Generating MASP Parameters...'
                                 : this.state.approval
@@ -241,7 +244,9 @@ ShieldedTransferDialog.propTypes = {
     value: PropTypes.object,
     revealPublicKey: PropTypes.object,
     tokensTransferAmount: PropTypes.string,
+    tokensTransferAmountValid: PropTypes.bool,
     tokensTransferAddress: PropTypes.string,
+    tokensTransferAddressValid: PropTypes.bool,
     tokensTransferMemo: PropTypes.string,
     shieldedData: PropTypes.object,
     disposableSigner: PropTypes.object,
@@ -256,7 +261,9 @@ const stateToProps = (state) => {
         value: state.assets.shieldedTokensTransferDialog.value,
         ibcTransferType: state.ibcTransfer.ibcTransferType.value,
         tokensTransferAmount: state.assets.tokensTransferAmount.value,
+        tokensTransferAmountValid: state.assets.tokensTransferAmount.valid,
         tokensTransferAddress: state.assets.tokensTransferAddress.value, 
+        tokensTransferAddressValid: state.assets.tokensTransferAddress.valid, 
         details: state.accounts.address.details,
         revealPublicKey: state.accounts.revealPublicKey.result,
         tokensTransferMemo: state.assets.tokensTransferMemo.value,
