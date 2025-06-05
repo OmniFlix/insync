eb08405792de337cceacaae40f500107f9a39591
SrikanthSoparla <srikanth.soparla@gmail.com>
2025-06-05 13:31:53 +0530

    processing state fixes, route updates.

--- src/components/ProcessingButton/index.js
+++ src/components/ProcessingButton/index.js
@@ -5,36 +5,10 @@ import './index.css';
 
 const ProcessingButton = ({ children }) => {
   return (
-    <p
-      className='processing_button'
-      sx={{
-        position: 'relative',
-        display: 'inline-block',
-        borderRadius: '24px',
-        padding: '3px', // Space for the animated border
-        background: 'conic-gradient(#FFF 0% 25%, transparent 25% 50%, #FFF 50% 75%, transparent 75% 100%)',
-        animation: 'spin 3s linear infinite',
-        backgroundSize: '400% 400%',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        overflow: 'visible',
-        width: '100%',
-      }}
-    >
-           {/* Button passed as children */}
+    <p className='processing_button'>
       <Box className='button_div'>
         {children}
       </Box>
-
-      {/* Keyframes */}
-      <style>
-        {`
-          @keyframes spin {
-            100% { background-position: 0% 0%; }
-            0% { background-position: 100% 100%; }
-          }
-        `}
-      </style>
     </p>
   );
 };
