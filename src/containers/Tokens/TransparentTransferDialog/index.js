fc576bfa752b3553b8fb1167ee9ba3f785b77c01
S.Srikanth Reddy <srikanth.soparla@gmail.com>
2025-06-12 11:24:53 +0530

    Update index.js

--- src/containers/Tokens/TransparentTransferDialog/index.js
+++ src/containers/Tokens/TransparentTransferDialog/index.js
@@ -78,7 +78,7 @@ class TransparentTransferDialog extends React.Component {
                 txs.token = this.props.feeOption?.fees?.token;
                 const tokenGasPrice = this.props.gasPrice.find((val) => val.token === this.props.feeOption?.fees?.token);
                 txs.feeAmount = new BigNumber(tokenGasPrice?.minDenomAmount);
-                if (props.feeOption?.fees?.token === config.TOKEN_ADDRESS) {
+                if (this.props.feeOption?.fees?.token === config.TOKEN_ADDRESS) {
                     txs.feeAmount = new BigNumber(0.000001);
                 }
             }
